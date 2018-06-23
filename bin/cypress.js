cf37d1830de7ab7a537e45fe0d20876d40d5b2c1
Trevor Pellegrini <trevorpellegrini@gmail.com>
2018-06-23 09:55:20 -0700

    model command in progress

--- bin/cypress.js
+++ bin/cypress.js
@@ -5,5 +5,5 @@ var CypressCommand = require('../lib/core')
 program
   .version('0.0.1', '-v, --version')
   .option('init', 'run math function', function(){ CypressCommand.init() })
-  .option('--model', 'run math function', function(){ console.log('model bro') })
+  .option('-m, --model', '', function(){ console.log('models in progress') })
   .parse(process.argv);
