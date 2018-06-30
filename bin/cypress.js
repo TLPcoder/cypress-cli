7d8f6b83bc2029c6e79b187e18b4f740eb3580f8
Trevor Pellegrini <trevorpellegrini@gmail.com>
2018-06-30 13:55:26 -0700

    edit route, middlewarepath, methods

--- bin/cypress.js
+++ bin/cypress.js
@@ -4,6 +4,7 @@ var CypressCommand = require('../lib/core')
 
 program
   .version('0.0.1', '-v, --version')
-  .option('init', 'run math function', function(){ CypressCommand.init() })
-  .option('-m, --model', '', function(){ console.log('models in progress') })
+  .option('-i, init', 'create cypress application', function(){ CypressCommand.init() })
+  .option('-c, create-route', 'create a route', function(){ CypressCommand.createRoute() })
+  .option('-e, edit-route', 'edit a route', function(){ CypressCommand.editRoute() })
   .parse(process.argv);
--- cypress/server/app.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const Cypress = require('cypress')
-const app = new Cypress()
-
-app.start()
